d81db21be37968f5a216667dda26abccf3f4716d
Alisha Bandyopadhyay <alishabandyopadhyay07@gmail.com>
2024-07-06 13:01:42 +0530

    feat: delete functionality

--- app/page.tsx
+++ app/page.tsx
@@ -6,17 +6,15 @@ const HomePage = () => {
   return (
     <>
       <div className="py-6">
-        <box>
-          <div className="flex items-center w-full justify-between">
-            <div>
-              <h1 className="text-xl font-medium text-gray-800">Tasks</h1>
-              <p className="font-light text-gray-400 text-md">
-                See all Task Listed Below
-              </p>
-            </div>
-            <Link href={"/new"}>New Task</Link>
+        <div className="flex items-center w-full justify-between">
+          <div>
+            <h1 className="text-xl font-medium text-gray-800">Tasks</h1>
+            <p className="font-light text-gray-400 text-md">
+              See all Task Listed Below
+            </p>
           </div>
-        </box>
+          <Link href={"/new"}>New Task</Link>
+        </div>
       </div>
     </>
   );
